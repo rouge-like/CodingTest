6967c445d8a7058bde4d313636035655f2a6bbe8
rouge-like <103567882+rouge-like@users.noreply.github.com>
2025-07-22 10:55:46 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string A;
+
+	while (getline(cin, A))
+	{
+		cout << A << "\n";
+	}
+}
