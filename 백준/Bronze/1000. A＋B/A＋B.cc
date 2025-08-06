1fd74b103f0db7ab82352a2bd366b2cb4b4f93e6
rouge-like <103567882+rouge-like@users.noreply.github.com>
2025-08-06 22:23:04 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    int a,b;
+    cin >> a >> b;
+    cout << a +b;
+}
