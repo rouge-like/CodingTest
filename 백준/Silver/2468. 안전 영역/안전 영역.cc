835abae68ebabb234da767962d0cffcf89ed9748
rouge-like <103567882+rouge-like@users.noreply.github.com>
2025-08-26 12:08:46 +0900

    [Silver I] Title: 안전 영역, Time: 28 ms, Memory: 2668 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int N;
+vector<vector<int>> grid(101, vector<int>(101, 0));
+vector<vector<bool>> visited(101, vector<bool>(101, false));
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int mxDepth = 0;
+void dfs(int y, int x, int depth)
+{
+	visited[y][x] = true;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+		if (visited[ny][nx] || grid[ny][nx] <= depth) continue;
+
+		dfs(ny, nx, depth);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> grid[i][j];
+			mxDepth = max(mxDepth, grid[i][j]);
+		}
+	}
+ 
+	int mxCnt = 0;
+
+	for (int d = 0; d < mxDepth; d++)
+	{
+		int cnt = 0;
+
+		for (int i = 0; i < N; i++)
+		{
+			fill(visited[i].begin(), visited[i].end(), false);
+		}
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				if (!visited[i][j] && grid[i][j] > d)
+				{
+					dfs(i, j, d);
+					cnt++;
+				}
+			}
+		}
+
+		mxCnt = max(mxCnt, cnt);
+	}
+
+	cout << mxCnt;
+}
