054b46843ff868f66fee6a5bba86369a1040e459
rouge-like <103567882+rouge-like@users.noreply.github.com>
2025-08-30 19:40:07 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+vector<int> visited(9, 0);
+vector<int> g;
+
+void dfs(int c)
+{
+	visited[c] = 1;
+	g.push_back(c);
+
+	if (g.size() == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << g[i] << " ";
+		
+		}
+		cout << "\n";
+	}
+	else
+	{
+		for (int i = c + 1; i <= n; i++)
+		{
+			dfs(i);
+		}
+	}
+
+	visited[c] = 0;
+	g.pop_back();
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+		dfs(i + 1);
+}
