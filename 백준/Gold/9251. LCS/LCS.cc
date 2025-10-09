7f0a40a2c565004af0a8c89154aec7007d3dc56e
rouge-like <103567882+rouge-like@users.noreply.github.com>
2025-10-09 23:38:22 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string A, B;
+	cin >> A >> B;
+	int n = A.size();
+	int m = B.size();
+
+	vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			if (A[i - 1] == B[j - 1])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+
+	cout << dp[n][m];
+}
