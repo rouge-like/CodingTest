bf0af5fa34d19be0195b9308ca2b158400c29ae6
rouge-like <103567882+rouge-like@users.noreply.github.com>
2026-03-10 14:02:29 +0900

    [level 3] Title: 베스트앨범, Time: 11.04 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cs
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cs
@@ -18,14 +18,13 @@ public class Solution {
     {
         Dictionary<string, int> sums = new Dictionary<string, int>();
         Dictionary<string, List<Music>> dict = new Dictionary<string, List<Music>>();
-        List<string> sortedGenres = new List<string>();
+        List<int> answer = new List<int>();
         
-        for(int i = 0; i < genres.Length; i++)
+        for(int i = 0; i < plays.Length; i++)
         {
             string g = genres[i];
             int p = plays[i];
-            
-            if (sums.ContainsKey(g))
+            if(dict.ContainsKey(g))
             {
                 sums[g] += p;
             }
@@ -33,29 +32,26 @@ public class Solution {
             {
                 sums.Add(g, p);
                 dict.Add(g, new List<Music>());
-                sortedGenres.Add(g);
             }
             dict[g].Add(new Music(i, p));
         }
+        var sorted = sums.OrderBy(x => x.Value).ToArray();
         
-        sortedGenres.Sort((a,b)=> sums[b].CompareTo(sums[a]));
-        
-        List<int> answer = new List<int>();
-        
-        foreach (string g in sortedGenres)
+        for(int i = sorted.Length - 1; i >= 0; i--)
         {
+            string g = sorted[i].Key;
             List<Music> list = dict[g];
-            list.Sort((a,b) => 
-                      {
-                         if (a.Plays == b.Plays) return a.Id.CompareTo(b.Id);
-                          return b.Plays.CompareTo(a.Plays);
-                      });
-            int cnt = 0;
-            foreach(Music m in list)
+            list.Sort((a,b) =>{
+               if(a.Plays == b.Plays) return a.Id.CompareTo(b.Id);
+               return b.Plays.CompareTo(a.Plays); 
+            });
+            int count = 0;
+            foreach(var m in list)
             {
+                if(count == 2) break;
+                
                 answer.Add(m.Id);
-                cnt++;
-                if (cnt == 2) break;
+                count++;
             }
         }
         return answer.ToArray();
